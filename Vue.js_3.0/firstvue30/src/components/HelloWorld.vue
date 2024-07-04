cb211e79ce90947c6452ee281e050fe1414a2fd3
feelins <feipengshao@163.com>
2024-07-04 22:45:57 +0800

    first vue3.0 example

---
+++ Vue.js_3.0/firstvue30/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup() {
+    const message = ref('Vue 3.0的欢迎信息')
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h2 {
+  margin: 40px 0 0;
+  color:chocolate;
+}
+h1 {
+  color:indianred;
+}
+</style>
